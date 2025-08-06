e1e03b1953512ec84b6bce230fb9fae834db456f
2420030503-pixel <2420030503@klh.edu.in>
2025-08-06 11:09:16 +0530

    Add files via upload
    6/8/2025

---
+++ AreaCircle2.java
@@ -0,0 +1,16 @@
+package Skill_3;
+
+class Circle1{
+    void calculateArea(double radius) {
+        double area = 3.14159 * radius * radius;
+        System.out.println("Radius: " + radius);
+        System.out.println("Area of the circle: " + area);
+    }
+}
+
+public class AreaCircle2{
+    public static void main(String[] args) {
+        Circle1 c = new Circle1();
+        c.calculateArea(2.5);  
+    }
+}
